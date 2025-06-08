21ad78b010b98e7668ca6cb85edf500ef9d5f5c2
harinibaskar <hbaskar@calpoly.edu>
2025-06-07 17:48:38 -0700

    message

--- src/main/java/hbaskar/api/TaigaStoryFetcher.java
+++ src/main/java/hbaskar/api/TaigaStoryFetcher.java
@@ -2,15 +2,15 @@
 package hbaskar.api;
 
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Map;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class TaigaStoryFetcher {
 	
@@ -27,7 +27,7 @@ public class TaigaStoryFetcher {
 			System.out.println(projectId);
 			// 1. Get all user stories
 			JSONArray stories = fetchUserStories(authToken, projectId);						
-			updateBacklogTotalPoints(authToken, stories, 5.0);
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
